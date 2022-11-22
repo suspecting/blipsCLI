db6bf9d8aa22e23414d7fe9d0862752de3f5a51b
suspecting <96511176+suspecting@users.noreply.github.com>
2022-11-22 01:11:29 -0600

    Now with 100% more feed, favoriting, and DMing!

--- blipsCLI.py
+++ blipsCLI.py
@@ -4,31 +4,61 @@
 
 import requests
 import argparse
+import feedparser
 
 parser = argparse.ArgumentParser(prog = 'blipsCLI', description='Blips from your command line.')
 parser.add_argument("-u", "--update", help="Sends update to Blips.")
-parser.add_argument("-c", "--credits", help="Credits for blipsCLI")
+parser.add_argument('-d','--direct', nargs='+', help='Sends Direct Message to Blips user.')
+parser.add_argument('-f', '--feed', action='store_true', help='Shows last 10 statuses.')
+parser.add_argument('-s', '--star', help='Star/Favorite a status by status number.')
 args = parser.parse_args()
 
-print(args.update) # preview
+print(args) # preview
 
-SigninUrl = 'https://blips.club/signin' # self explanatory
-StatusUrl = 'https://blips.club/status/create' # self explanatory
+SigninUrl = 'https://blips.club/signin'
+StatusUrl = 'https://blips.club/status/create'
+DirectUrl = 'https://blips.club/direct_messages/create'
+FavoriteUrl = 'https://blips.club/favorites/create'
 
 
-payload = { # used for authentication
-    'username': 'Exampleaccount', # Edit this field
-    'password': 'Donteventrytologin', # Edit this field
+AuthPayload = { # used for authentication
+    'username': 'testing', # Edit this field
+    'password': 'helloworld', # Edit this field
 }
 
-session = requests.session() # opens session with said information
+session = requests.session()
 resp = session.get(SigninUrl) 
 cookie_session = resp.cookies['PHPSESSID']
-r = session.post(SigninUrl, data=payload)
+AuthResponse = session.post(SigninUrl, data=AuthPayload)
 
-payload2 = {
-    'session': cookie_session,
-    'status': args.update, 
-}
 
-r2 = session.post(StatusUrl, data=payload2)
+if args.update:
+    UpdatePayload = {
+        'session': cookie_session,
+        'status': args.update, 
+    }
+    
+    UpdateResponse = session.post(StatusUrl, data=UpdatePayload)
+
+if args.direct:
+    DirectPayload = {
+        'session': cookie_session,
+        'status': args.direct[1],
+        'recipient': args.direct[0]
+    }
+    
+    DirectResponse = session.post(DirectUrl, data=DirectPayload)
+
+if args.feed:
+    Feed = feedparser.parse("https://blips.club/" + AuthPayload['username'] + "/with_friends.rss")
+    for i in range(10):
+        PostId = Feed.entries[i].link.replace('https://blips.club/testing/statuses/', '')
+        print(Feed.entries[i].title + " / Status #" + PostId)
+
+if args.star:
+    FavoritePayload = {
+        'session': cookie_session,
+        'id': args.star,
+    }
+    
+    FavoriteResponse = session.post(FavoriteUrl, data=FavoritePayload)
